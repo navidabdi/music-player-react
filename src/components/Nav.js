1e5d6fcd928f00422fcf0b6bf4563c83ed9929d6
navidabdi <artsanaat@gmail.com>
2022-02-06 12:18:16 +0330

    Add favicon and a little responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,19 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
 import { useStateContext } from '../contexts/StateMusics';
 import DarkModeIcon from './DarkModeIcon';
+import logo from '../img/logo.png';
 const Nav = () => {
   // Context
   const { libraryStatus, setLibraryStatus } = useStateContext();
   return (
     <nav>
-      <a href="/">
+      <a className="logo" href="/">
+        <img src={logo} alt="" />
         <h1>Waves Player</h1>
       </a>
       <div className="buttons">
         <DarkModeIcon />
         <button onClick={() => setLibraryStatus(!libraryStatus)}>
           Library
-          <FontAwesomeIcon icon={faMusic} />
+          <FontAwesomeIcon className="library-icon" icon={faMusic} />
         </button>
       </div>
     </nav>
